2b632fb7b9ccf55744b59a75ea0e702c605cfb7b
Raphael Nestler <raphael.nestler@renuo.ch>
2025-10-16 20:55:37 +0200

    Separate create_pdfs script for 2025

---
+++ 2025/create_pdfs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+rm -f speaker-notes-day1-morning.pdf
+rm -f speaker-notes-day1-afternoon.pdf
+
+rm -f speaker-notes-day2-morning.pdf
+rm -f speaker-notes-day2-afternoon.pdf
+
+for file in ./day-[12]/*.md; do
+    echo $file
+    pandoc -V geometry:margin=2cm --from markdown+raw_tex --to pdf "$file" -o "${file%.*}.pdf" &
+done
+
+wait
+
+pdftk ./day-1/speaker-notes-[1-4].pdf cat output speaker-notes-day1-morning.pdf
+pdftk ./day-1/speaker-notes-[5-8].pdf cat output speaker-notes-day1-afternoon.pdf
+
+pdftk ./day-2/speaker-notes-[1-4].pdf cat output speaker-notes-day2-morning.pdf
+pdftk ./day-2/speaker-notes-[5-8].pdf cat output speaker-notes-day2-afternoon.pdf
+
